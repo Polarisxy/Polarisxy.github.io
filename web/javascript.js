89635c2d3eb0cd958711d3bbb99c7a5060fde387
Polarisxy <38269616+Polarisxy@users.noreply.github.com>
2018-04-18 03:36:39 +0000

    Update javascript.js

--- web/javascript.js
+++ web/javascript.js
@@ -1,6 +1,6 @@
-function showpic(whichpic){
+function showPic(whichpic){
   var source=whichpic.getAttribute("href");
-  var placeholder=document.getElementById("placeholder");
+  var placeholder = document.getElementById("placeholder");
   placeholder.setAttribute("src",source);
 
 }
